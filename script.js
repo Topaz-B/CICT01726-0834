e6af9c8ca83d2983e5380facacd471a56f7df2ca
Topaz-B <neelsilus@gmail.com>
2026-09-09 09:04:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-function calculateGrade(marks) {
-  if(
+function changeColor(){ 
+    document.getElementById("home").style.color ="Red"; 
+}
+function makeRed(){
+	document.getElementById("home").style.color="Red";
+}
+function makeBlue(){
+    document.getElementById("home").style.color="Blue";
+}
+function makeGreen(){
+	document.getElementById("home").style.color="Green";
+}
+function yellowBackground(){
+	document.body.style.backgroundColor ="yellow";
+}
+function blueBackground(){
+	document.body.style.backgroundColor ="blue";
+}
+function whiteBackground(){
+	document.body.style.backgroundColor ="white";
+}
+function darkmode(){
+	document.body.style.backgroundColor ="black";
+	document.body.style.color ="white"
+}
+function lightmode(){
+	document.body.style.backgroundColor ="white";
+	document.body.style.color ="black";
+}
+function changeMessage(){
+	document.getElementById("message").innerHTML =
+	"Thank you for visiting my website!";
+}
